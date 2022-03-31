2c51a3db05d6b0f1fa84449bb454697cd7cc28d6
PedroVViana <86854663+PedroVViana@users.noreply.github.com>
2022-03-31 14:09:54 -0300

    Add files via upload

---
+++ FormO-pitblast/FormO-pitblast/Helpers/Help.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FormO_pitblast.Helpers
+{
+
+    public static class Help
+    {
+       
+        public static void FecharTelaCadastro()
+        {
+            var tela = Application.Current.MainWindow;
+            tela.Close();
+        }
+
+
+    }
+}
